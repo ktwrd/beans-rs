b6b844a835b389c918fc025d88666a8b0f67da27
kate <kate@dariox.club>
2024-05-25 16:42:36 +0800

    Set console window

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use beans_rs::wizard;
 
 #[tokio::main]
 async fn main() {
+    #[cfg(target_os = "windows")]
+    let _ = winconsole::console::set_title(format!("beans v{}", beans_rs::VERSION).as_str());
+
     wizard::WizardContext::run().await;
 }
 
