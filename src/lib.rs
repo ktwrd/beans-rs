f86148f2f43a626de33ad8d4c984dde147c73f1e
kate <kate@dariox.club>
2024-05-25 11:38:16 +0800

    Move RunnerContext to it's own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 use const_format::concatcp;
-use crate::helper::{find_sourcemod_path, InstallType};
-use crate::version::{RemotePatch, RemoteVersion, RemoteVersionResponse};
-use crate::wizard::BeansError;
 
 pub mod depends;
 pub mod helper;
 pub mod wizard;
 pub mod version;
+mod ctx;
+pub use ctx::*;
 
 
 
@@ -27,70 +26,3 @@ pub const ARIA2C_BINARY: &[u8] = include_bytes!("../Binaries/aria2c");
 pub const BUTLER_BINARY: &[u8] = include_bytes!("../Binaries/butler.exe");
 #[cfg(not(windows))]
 pub const BUTLER_BINARY: &[u8] = include_bytes!("../Binaries/butler");
-
-
-#[derive(Debug, Clone)]
-pub struct RunnerContext
-{
-    pub sourcemod_path: String,
-    pub remote_version_list: RemoteVersionResponse,
-    pub current_version: Option<usize>
-}
-impl RunnerContext
-{
-    pub async fn create_auto() -> Result<Self, BeansError>
-    {
-        depends::try_write_deps();
-        depends::try_install_vcredist();
-        let sourcemod_path = match find_sourcemod_path() {
-            Some(v) => v,
-            None => {
-                return Err(BeansError::SourceModLocationNotFound);
-            }
-        };
-        let version_list = crate::version::get_version_list().await;
-
-        if helper::install_state() == InstallType::OtherSource {
-            version::update_version_file();
-        }
-
-        return Ok(Self
-        {
-            sourcemod_path,
-            remote_version_list: version_list,
-            current_version: crate::version::get_current_version()
-        });
-    }
-
-    /// Get the latest item in `remote_version_list`
-    pub fn latest_remote_version(&mut self) -> (usize, RemoteVersion)
-    {
-        let mut highest = usize::MIN;
-        for (key, _) in self.remote_version_list.clone().versions.into_iter() {
-            if key > highest {
-                highest = key;
-            }
-        }
-        let x = self.remote_version_list.versions.get(&highest).unwrap();
-        (highest, x.clone())
-    }
-
-    /// When self.current_version is some, iterate through patches and fetch the patch that is available
-    /// to bring the current version in-line with the latest version.
-    pub fn has_patch_available(&mut self) -> Option<RemotePatch>
-    {
-        let current_version = self.current_version.clone();
-        let (remote_version, _) = self.latest_remote_version();
-        match current_version {
-            Some(cv) => {
-                for (_, patch) in self.remote_version_list.clone().patches.into_iter() {
-                    if patch.file == format!("of-{}to{}.pwr", cv, remote_version) {
-                        return Some(patch);
-                    }
-                }
-                return None;
-            },
-            _ => None
-        }
-    }
-}
