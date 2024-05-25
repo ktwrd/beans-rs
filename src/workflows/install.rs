87b752ee2d49ff6468e97c254c725ec20a598a5e
kate <kate@dariox.club>
2024-05-25 16:11:46 +0800

    Update install.rs

--- src/workflows/install.rs
+++ src/workflows/install.rs
@@ -50,7 +50,7 @@ impl InstallWorkflow {
         } else {
             eprintln!("Not writing .adastral since the version wasn't provided");
         }
-        println!("Done! Make sure to restart steam before playing");
+        println!("Done! Make sure that the following is done;\n  1. Install Source SDK Base 2013 Multiplayer\n  2. Restart Steam\n  3.Play Open Fortress!");
         Ok(())
     }
 }
