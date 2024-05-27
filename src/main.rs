f4341fe6d148de68a0da83d9ef8cbf2e6e6e30d5
kate <kate@dariox.club>
2024-05-27 09:51:09 +0800

    Add ability to enable debug mode with --debug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use clap::{Arg, Command};
 use beans_rs::wizard;
 
 #[tokio::main]
 async fn main() {
     #[cfg(target_os = "windows")]
     let _ = winconsole::console::set_title(format!("beans v{}", beans_rs::VERSION).as_str());
+    #[cfg(debug_assertions)]
+    unsafe { beans_rs::FORCE_DEBUG = true; }
+
+    let cmd = Command::new("beans-rs")
+        .version(clap::crate_version!())
+        .bin_name(clap::crate_name!())
+        .arg(Arg::new("debug")
+            .long("debug")
+            .help("Enable debug logging")
+            .action(clap::ArgAction::SetTrue));
+    let matches = cmd.get_matches();
+    // println!("{:#?}", matches);
+    if matches.get_flag("debug") {
+        println!("[beans_rs::main] Debug mode enabled");
+        unsafe { beans_rs::FORCE_DEBUG = true; }
+    }
 
     wizard::WizardContext::run().await;
 }
