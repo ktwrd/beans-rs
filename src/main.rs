73053fa9a2eafb398d0e2a316b3456254693a8ff
kate <kate@dariox.club>
2024-05-27 21:27:34 +0800

    Add subcommand to manually install from zstd

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::str::FromStr;
 use clap::{Arg, Command};
-use beans_rs::wizard;
+use beans_rs::{helper, RunnerContext, wizard};
+use beans_rs::workflows::InstallWorkflow;
 
 #[tokio::main]
 async fn main() {
@@ -14,7 +16,23 @@ async fn main() {
         .arg(Arg::new("debug")
             .long("debug")
             .help("Enable debug logging")
-            .action(clap::ArgAction::SetTrue));
+            .action(clap::ArgAction::SetTrue))
+        .subcommand(Command::new("manual_install")
+            .about("Manually install by specifying archive location and version.")
+            .arg(
+                Arg::new("location")
+                    .help(".tar.zstd file location")
+                    .long("location")
+                    .action(clap::ArgAction::Set)
+                    .required(true)
+            )
+            .arg(
+                Arg::new("version")
+                    .help("Version number")
+                    .long("version")
+                    .action(clap::ArgAction::Set)
+                    .required(true)
+            ));
     let matches = cmd.get_matches();
     // println!("{:#?}", matches);
     if matches.get_flag("debug") {
@@ -22,7 +40,45 @@ async fn main() {
         unsafe { beans_rs::FORCE_DEBUG = true; }
     }
 
-    wizard::WizardContext::run().await;
+    match matches.subcommand() {
+        Some(("manual_install", mi_matches)) => {
+            let loc = mi_matches.get_one::<String>("location").unwrap();
+            let v = mi_matches.get_one::<String>("version").unwrap();
+            let version = match usize::from_str(v) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("Failed to parse version argument: {:}", e);
+                    if helper::do_debug() {
+                        eprintln!("{:#?}", e);
+                    }
+                    return;
+                }
+            };
+            let ctx = match RunnerContext::create_auto().await {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("{:}", e);
+                    if helper::do_debug() {
+                        eprintln!("======== Full Error ========");
+                        eprintln!("{:#?}", e);
+                    }
+                    std::process::exit(0);
+                }
+            };
+            let smp_x = ctx.sourcemod_path.clone();
+            if let Err(e) = InstallWorkflow::install_from(loc.clone(), smp_x, Some(version)).await {
+                println!("Failed to run InstallWorkflow::install_from! {:}", e);
+                if helper::do_debug() {
+                    eprintln!("{:#?}", e);
+                }
+            }
+            let _ = helper::get_input("Press enter/return to exit");
+            std::process::exit(0);
+        },
+        _ => {
+            wizard::WizardContext::run().await;
+        }
+    }
 }
 
 
