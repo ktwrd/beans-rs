7ce4ee5e5c53b22a2f3a11c79d4d8ec3987d77e7
kate <kate@dariox.club>
2024-06-01 17:38:23 +0800

    Create flags.rs

---
+++ src/flags.rs
@@ -0,0 +1,56 @@
+use bitflags::bitflags;
+bitflags!
+{
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub struct LaunchFlag: u32
+    {
+        // debug mode, print full errors and other debug messages to console.
+        const DEBUG_MODE = 0x01;
+        // run from beans script
+        const AUTOMATED = 0x02;
+        // use the standard CLI wizard
+        const WIZARD = 0x04;
+        // enable experimental GUI for the wizard
+        const WIZARD_GUI = 0x08;
+        // please enable this flag when this is being used by a standalone application
+        const STANDALONE_APP = 0x16;
+    }
+}
+pub static mut LAUNCH_FLAGS: u32 = 0x00;
+/// check if the `flag` provided is in `LAUNCH_FLAGS`
+pub fn has_flag(flag: LaunchFlag) -> bool
+{
+    unsafe {
+        let data = LaunchFlag::from_bits(LAUNCH_FLAGS).unwrap_or(LaunchFlag::empty());
+        data.contains(flag)
+    }
+}
+/// Add a flag to `LAUNCH_FLAGS`
+pub fn add_flag(flag: LaunchFlag)
+{
+    unsafe {
+        let mut data = LaunchFlag::from_bits(LAUNCH_FLAGS).unwrap_or(LaunchFlag::empty());
+        data.insert(flag);
+        LAUNCH_FLAGS = data.bits();
+    }
+}
+/// remove a flag from `LAUNCH_FLAGS`
+pub fn remove_flag(flag: LaunchFlag)
+{
+    unsafe {
+        let mut data = LaunchFlag::from_bits(LAUNCH_FLAGS).unwrap_or(LaunchFlag::empty());
+        data.remove(flag);
+        LAUNCH_FLAGS = data.bits();
+    }
+}
+
+#[cfg(debug_assertions)]
+pub fn debug_mode() -> bool
+{
+    true
+}
+#[cfg(not(debug_assertions))]
+pub fn debug_mode() -> bool
+{
+    has_flag(LaunchFlag::DEBUG_MODE)
+}
