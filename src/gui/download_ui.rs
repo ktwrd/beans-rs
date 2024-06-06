d32c35bf55a8e541f0a1796816913d456656b836
kate <kate@dariox.club>
2024-06-06 20:41:48 +0800

    Add fltk as a dependency, and add window for download progress

---
+++ src/gui/download_ui.rs
@@ -0,0 +1,7 @@
+﻿#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(clippy::needless_update)]
+
+include!(concat!(env!("OUT_DIR"), "/download_ui.rs"));
---
+++ src/gui/mod.rs
@@ -0,0 +1,20 @@
+﻿use fltk::prelude::WidgetExt;
+use fltk::window::Window;
+
+pub mod download_ui;
+#[allow(dead_code)]
+#[derive(Copy, Clone)]
+pub enum GUIAppStatus {
+    Update,
+    Quit,
+}
+/// Make the `window` provided the in be the center of the current screen.
+pub fn window_centre_screen(window: &mut Window) {
+    let (sx, sy) = fltk::app::screen_coords();
+    let width = window.width();
+    let height = window.height();
+    let (mut x, mut y) = fltk::app::screen_size().clone();
+    x -= width.clone() as f64;
+    y -= height.clone() as f64;
+    window.resize(((x / 2.0) as i32) + sx, ((y / 2.0) as i32) + sy, width, height);
+}
