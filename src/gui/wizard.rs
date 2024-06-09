4ac262e509c676b31ff1d3c0c65a41708b03ff08
kate <kate@dariox.club>
2024-06-09 11:32:36 +0800

    Fully implement the update task

--- src/gui/wizard.rs
+++ src/gui/wizard.rs
@@ -1,11 +1,13 @@
 use async_recursion::async_recursion;
 use fltk::{*, prelude::*};
 use log::{debug, error, trace};
-use crate::{RunnerContext, BeansError, gui};
+use crate::{RunnerContext, BeansError, gui, helper};
 use crate::appvar::AppVarData;
+use crate::gui::dialog_confirm::{ConfirmUpdateDialogDetails, DialogConfirmType, DialogResult};
 use crate::gui::GUIAppStatus;
 use crate::gui::install_confirm::InstallConfirmResult;
 use crate::gui::wizard_ui::WizardInterface;
+use crate::version::RemoteVersion;
 use crate::workflows::{InstallWorkflow, UpdateWorkflow};
 
 #[async_recursion]
@@ -48,61 +50,112 @@ pub async fn run(ctx: &mut RunnerContext) {
             }
         }
     }
+    let mut x = ctx.clone();
     match final_action {
         Some(GUIAppStatus::WizardBtnInstall) => {
-            let (i, r) = ctx.latest_remote_version();
-            trace!("[gui::wizard::run] Showing confirm dialog for v{}", i);
-            match gui::install_confirm::run(ctx, i, r).await {
-                InstallConfirmResult::Continue => {
-                    let mut iwf = InstallWorkflow {
-                        context: ctx.clone()
-                    };
-                    trace!("[gui::wizard::run] User wants to install! Calling RunnerContext");
-                    match iwf.install_version(i).await {
-                        Ok(_) => {
-                            gui::install_complete::run();
-                            // This is done so we don't prompt the user once the GUI has closed.
-                            unsafe {crate::HEADLESS = true;}
-                        },
-                        Err(e) => {
-                            error!("[gui::wizard::run] Failed to run InstallWorkflow::install_version({i}) {:#?}", e);
-                            panic!("[gui::wizard::run] Failed to run InstallWorkflow::install_version({i}) {:#?}", e);
-                        }
-                    };
-                },
-                InstallConfirmResult::Cancel => {
-                    debug!("[gui::wizard::run] User clicked on the Cancel button, showing Wizard again");
-                    run(ctx).await;
-                }
-            }
+            btn_install_latest(&mut x).await;
         },
         Some(GUIAppStatus::WizardBtnUpdate) => {
-            let (i, r) = ctx.latest_remote_version();
-            if let Some(ci) = ctx.current_version {
-                if i >= ci {
-                    gui::update_alreadylatest::run(ci, i);
-                    // This is done so we don't prompt the user once the GUI has closed.
-                    unsafe {crate::HEADLESS = true;}
-                    return;
-                }
-            } else {
-                gui::dialog_notinstalled::run("Update");
-                // This is done so we don't prompt the user once the GUI has closed.
-                unsafe {crate::HEADLESS = true;}
-                return;
-            }
-            if ctx.has_patch_available().is_none() {
-                gui::update_nopatchavailable::run(i);
-                // This is done so we don't prompt the user once the GUI has closed.
-                unsafe {crate::HEADLESS = true;}
-                return;
-            }
-
-            let mut uwf = UpdateWorkflow {
-                ctx: ctx.clone()
-            };
-            trace!("[gui::wizard::run] User wants to update! Calling UpdateWorkflow");
+            btn_update(&mut x).await;
         }
         _ => {}
+    }
+}
+/// Install (or reinstall) the latest version.
+async fn btn_install_latest(ctx: &mut RunnerContext) {
+    let (i, r) = ctx.latest_remote_version();
+    trace!("[gui::wizard::btn_install] Showing confirm dialog for v{}", i);
+    btn_install(ctx, i, r).await;
+}
+async fn btn_install(ctx: &mut RunnerContext, id: usize, details: RemoteVersion) {
+    // TODO rewrite so it uses `dialog_confirm`.
+    match gui::install_confirm::run(ctx, id, details).await {
+        InstallConfirmResult::Continue => {
+            let mut iwf = InstallWorkflow {
+                context: ctx.clone()
+            };
+            trace!("[gui::wizard::btn_install] User wants to install! Calling RunnerContext");
+            match iwf.install_version(id).await {
+                Ok(_) => {
+                    gui::install_complete::run();
+                    // This is done so we don't prompt the user once the GUI has closed.
+                    unsafe {crate::HEADLESS = true;}
+                },
+                Err(e) => {
+                    error!("[gui::wizard::btn_install] Failed to run InstallWorkflow::install_version({id}) {:#?}", e);
+                    panic!("[gui::wizard::btn_install] Failed to run InstallWorkflow::install_version({id}) {:#?}", e);
+                }
+            };
+        },
+        InstallConfirmResult::Cancel => {
+            debug!("[gui::wizard::btn_install] User clicked on the Cancel button, showing Wizard again");
+            run(ctx).await;
+        }
+    }
+}
+async fn btn_update(ctx: &mut RunnerContext) {
+    let av = AppVarData::get();
+    let (i, _) = ctx.latest_remote_version();
+    if let Some(ci) = ctx.current_version {
+        if i <= ci {
+            // dialog::alert_default(&format!("Latest version of {} is already installed (v{})", av.mod_info.name_stylized, ci));
+            // gui::update_alreadylatest::run(ci, i);
+            gui::dialog_generic::run(
+                &format!("beans - Update {}", &av.mod_info.name_stylized),
+                &format!("Latest version of {} is already installed (v{})", &av.mod_info.name_stylized, ci)
+            );
+            // This is done so we don't prompt the user once the GUI has closed.
+            unsafe {crate::HEADLESS = true;}
+            return;
+        }
+    } else {
+        // gui::dialog_notinstalled::run("Update");
+        gui::dialog_generic::run(
+            &format!("beans - Update {}", &av.mod_info.name_stylized),
+            &format!("Unable to update since {} is not installed.", &av.mod_info.name_stylized)
+        );
+        // This is done so we don't prompt the user once the GUI has closed.
+        unsafe {crate::HEADLESS = true;}
+        return;
+    }
+
+    let target_patch = match ctx.has_patch_available() {
+        Some(p) => p,
+        None => {
+            // dialog::alert_default(&format!("No patch available for v{}. Please re-install.", i));
+            gui::update_nopatchavailable::run(i);
+            // This is done so we don't prompt the user once the GUI has closed.
+            unsafe {crate::HEADLESS = true;}
+            return;
+        }
+    };
+
+    let mut ct = DialogConfirmType::Update(ConfirmUpdateDialogDetails {
+        id_target: i,
+        id_source: ctx.current_version.unwrap_or(0),
+        dl_size: 0,
+        details: target_patch.clone()
+    });
+    match gui::dialog_confirm::run(ctx, &mut ct).await {
+        DialogResult::Continue => {
+            trace!("[gui::wizard::btn_update] User wants to update! Calling UpdateWorkflow");
+            match UpdateWorkflow::wizard(ctx).await {
+                Ok(()) => {
+                    gui::dialog_generic::run(
+                        &format!("beans - Update {}", &av.mod_info.name_stylized),
+                        &format!("{} has successfully been updated to v{}", &av.mod_info.name_stylized, i)
+                    );
+                },
+                Err(e) => {
+                    let (x, y) = gui::get_center_screen();
+                    error!("[gui::wizard::btn_update] Failed to run UpdateWorkflow::wizard {:#?}", e);
+                    dialog::message(x, y, &format!("Failed to Update. {:}", e));
+                }
+            };
+        },
+        _ => {
+            debug!("[gui::wizard::btn_update] User clicked on the Cancel button, showing Wizard again");
+            run(ctx).await;
+        }
     }
 }
