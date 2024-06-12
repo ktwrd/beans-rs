366996912bc5e7f69bfb6eaaafe370a4265c9dbf
kate <kate@dariox.club>
2024-06-12 17:27:54 +0800

    Rename set_instance to set_instance_blank

--- src/userconfig.rs
+++ src/userconfig.rs
@@ -44,7 +44,7 @@ impl UserConfigData {
     ///
     /// NOTE panics when `serde_json::from_str()` is Err, or when `JSON_DATA.read()` is Err.
     /// REMARKS does not set `UCD_INSTANCE` to generated data, since this is only done by
-    /// `UserConfigData::set_instance()`.
+    /// `UserConfigData::set_instance_blank()`.
     pub fn parse() -> Self {
         debug!("[UserConfigData::parse] trying to get JSON_DATA");
         let x = JSON_DATA.read();
@@ -59,7 +59,7 @@ impl UserConfigData {
     }
 
     /// Try and read the data from `UCD_INSTANCE` and return when some.
-    /// Otherwise, when it's none, we return `UserConfigData::set_instance()`
+    /// Otherwise, when it's none, we return `UserConfigData::set_instance_blank()`
     ///
     /// NOTE this function panics when Err on `UCD_INSTANCE.read()`.
     pub fn get() -> Self {
@@ -75,22 +75,22 @@ impl UserConfigData {
             panic!("[UserConfigData::get] Failed to read UCD_INSTANCE {:#?}", e);
         }
 
-        Self::set_instance()
+        Self::set_instance_blank()
     }
 
     /// Set the content of `UCD_INSTANCE` to the result of `UserConfigData::parse()`
     ///
     /// NOTE this function panics when Err on `UCD_INSTANCE.write()`
-    pub fn set_instance() -> Self {
+    pub fn set_instance_blank() -> Self {
         let instance = Self::parse();
 
         match UCD_INSTANCE.write() {
             Ok(mut data) => {
                 *data = Some(instance.clone());
-                debug!("[set_instance] set content of AVD_INSTANCE to {:#?}", instance);
+                debug!("[set_instance_blank] set content of AVD_INSTANCE to {:#?}", instance);
             },
             Err(e) => {
-                panic!("[set_instance] Failed to set AVD_INSTANCE! {:#?}", e);
+                panic!("[set_instance_blank] Failed to set AVD_INSTANCE! {:#?}", e);
             }
         }
 
