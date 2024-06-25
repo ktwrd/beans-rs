3e783c145ab55a60dd05fdf19740b54755438bde
kate <kate@dariox.club>
2024-06-25 12:55:39 +0800

    Add function prompt_confirm

--- src/workflows/install.rs
+++ src/workflows/install.rs
@@ -1,5 +1,6 @@
-use log::{debug, error, warn};
+use log::{debug, error, info, warn};
 use crate::{DownloadFailureReason, helper, RunnerContext};
+use crate::appvar::AppVarData;
 use crate::BeansError;
 use crate::version::{AdastralVersionFile, RemoteVersion};
 
@@ -16,6 +17,44 @@ impl InstallWorkflow {
         }
 
         Self::install_with_remote_version(ctx, latest_remote_id, latest_remote).await
+    }
+
+    /// Prompt the user to confirm if they want to reinstall (when parameter `current_version` is Some)
+    ///
+    /// Will always return `true` when `crate::PROMPT_DO_WHATEVER` is `true`.
+    ///
+    /// Returns: `true` when the installation should continue, `false` when we should silently abort.
+    pub fn prompt_confirm(current_version: Option<usize>) -> bool
+    {
+        unsafe {
+            if crate::PROMPT_DO_WHATEVER {
+                info!("[InstallWorkflow::prompt_confirm] skipping since PROMPT_DO_WHATEVER is true");
+                return  true;
+            }
+        }
+        let av = AppVarData::get();
+        if let Some(v) = current_version {
+            println!("[InstallWorkflow::prompt_confirm] Seems like {} is already installed (v{})", v, av.mod_info.name_stylized);
+
+            println!("Are you sure that you want to reinstall?");
+            println!("Yes/Y (default)");
+            println!("No/N");
+            let user_input = helper::get_input("-- Enter option below --");
+            match user_input.to_lowercase().as_str() {
+                "y" | "yes" | "" => {
+                    true
+                },
+                "n" | "no" => {
+                    false
+                },
+                _ => {
+                    println!("Unknown option \"{}\"", user_input.to_lowercase());
+                    Self::prompt_confirm(current_version)
+                }
+            }
+        } else {
+            true
+        }
     }
 
     /// Install the specified version by its ID to the output directory.
