acc79d60b97eaa03fc6c6ed7501b649227d1610f
kate <kate@dariox.club>
2025-03-08 10:57:55 +0800

    [VerifyWorkflow] Add post-verify message

--- src/workflows/verify.rs
+++ src/workflows/verify.rs
@@ -1,3 +1,6 @@
+use log::{error,
+          debug};
+
 use crate::{appvar::AppVarData,
             butler,
             helper,
@@ -30,21 +33,21 @@ impl VerifyWorkflow
         let remote: RemoteVersion = ctx.current_remote_version()?;
         if remote.signature_url.is_none()
         {
-            eprintln!(
+            error!(
                 "[VerifyWorkflow::wizard] Couldn't get signature URL for version {}",
                 current_version_id
             );
         }
         if remote.heal_url.is_none()
         {
-            eprintln!(
+            error!(
                 "[VerifyWorkflow::wizard] Couldn't get heal URL for version {}",
                 current_version_id
             );
         }
         if remote.signature_url.is_none() || remote.heal_url.is_none()
         {
-            eprintln!("[VerifyWorkflow::wizard] Unable to update, missing remote files!");
+            error!("[VerifyWorkflow::wizard] Unable to update, missing remote files!");
             return Ok(());
         }
 
@@ -59,8 +62,31 @@ impl VerifyWorkflow
             mod_dir_location.clone(),
             format!("{}{}", &av.remote_info.base_url, remote.heal_url.unwrap())
         )?;
-        println!("[VerifyWorkflow::wizard] The verification process has completed, and any corruption has been repaired.");
+        Self::post_verify_msg();
         ctx.gameinfo_perms()?;
         Ok(())
     }
+    fn post_verify_msg()
+    {
+        let av = AppVarData::get();
+        println!("{}", av.sub(VERIFY_FINISH_MSG.to_string()));
+        debug!("[VerifyWorkflow::post_verify_msg] Displayed INSTALL_FINISH_MSG");
+        
+        #[cfg(target_os = "windows")]
+        winconsole::window::show(true);
+        #[cfg(target_os = "windows")]
+        winconsole::window::flash(winconsole::window::FlashInfo {
+            count: 0,
+            flash_caption: true,
+            flash_tray: true,
+            indefinite: false,
+            rate: 0,
+            until_foreground: true
+        });
+    }
 }
+
+#[cfg(not(target_os = "windows"))]
+pub const VERIFY_FINISH_MSG: &str = include_str!("../text/verify_complete_linux.txt");
+#[cfg(target_os = "windows")]
+pub const VERIFY_FINISH_MSG: &str = include_str!("../text/verify_complete_windows.txt");
